8ffe68b19a1f7484bfa42dd58afaef7332b57102
umar <umarmaniyar232@gmail.com>
2024-11-29 16:15:18 +0000

    final script code

--- backups.sh
+++ backups.sh
@@ -3,5 +3,5 @@ source_dir="/home/umaru/source"
 backup_dir="/home/umaru/user/backups"
 time_stamp=$(date +"%Y%m%d%H%M")
 
-tar -czf "$backup_dir/backup_$timestamp.tar.gz "$source_dir"
+tar -czf "$backup_dir/backup_$time_stamp.tar.gz" "$source_dir"
 echo "the backups are stored at $backup_dir/backup_$time_stamp.tar.gz"
