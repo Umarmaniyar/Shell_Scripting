0041562d4ba019fe8a75045ec09265f1f253022b
umar <umarmaniyar232@gmail.com>
2024-11-29 13:55:03 +0000

    added comprssing code

--- backups.sh
+++ backups.sh
@@ -2,3 +2,6 @@
 source_dir="/home/umaru/source"
 backup_dir="/home/umaru/user/backups"
 time_stamp=$(date +"%Y%m%d%H%M")
+
+tar -czf "$backup_dir/backup_$timestamp.tar.gz "$source_dir"
+echo "the backups are stored at $backup_dir/backup_$time_stamp.tar.gz"
